686e8ca3c1e909aa8a357ec34f76f48cec80c64b
Thomas Gläßle <t_glaessle@gmx.de>
2017-08-10 21:38:28 +0200

    Release pycbox 0.0.1 (first release)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pycbox',
-    version='0.0.0',
+    version='0.0.1',
     description='Web interface for directory listings and picture gallery',
     long_description=None,
     author='Thomas Gläßle',
